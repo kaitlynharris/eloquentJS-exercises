660bd62c13fe4f78d3ebee26fac406d735346b84
kaitlynharris <kaitlynhikes@gmail.com>
2016-06-13 11:34:50 -0700

    every and then some

--- chapter5/exercises.js
+++ chapter5/exercises.js
@@ -56,3 +56,28 @@ var ancestorsByCentury = groupBy(ancestry, byCentury);
 for (var century in ancestorsByCentury) {
   console.log(century, ":", average(ancestorsByCentury[century].map(ageOfDeath)));
 }
+
+// every and then some
+
+function every(array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if (!predicate(array[i])) { return false; }
+  }
+  return true;
+}
+
+function some(array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if (predicate(array[i])) { return true; }
+  }
+  return false;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
