644ac56eadf5790e71afe5d17fbccbf338e2a19d
kaitlynharris <kaitlynhikes@gmail.com>
2016-06-15 16:23:27 -0700

    last problem

--- chapter6/exercises.js
+++ chapter6/exercises.js
@@ -23,26 +23,73 @@ Object.defineProperty(Vector.prototype, "length", {
   }
 });
 
-console.log(new Vector(1, 2).plus(new Vector(2, 3)));
-// → Vector{x: 3, y: 5}
-console.log(new Vector(1, 2).minus(new Vector(2, 3)));
-// → Vector{x: -1, y: -1}
-console.log(new Vector(3, 4).length);
-// → 5
+// console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// // → Vector{x: 3, y: 5}
+// console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// // → Vector{x: -1, y: -1}
+// console.log(new Vector(3, 4).length);
+// // → 5
 
 // another cell
 // didn't feel like including all the book code defining TextCell etc.
 // this won't run on its own but it will run in the sandbox
 
-function StretchCell(inner, width, height) {
-  this.inner = inner;
-  this.minWidth = function () {
-    return Math.max(this.inner.minWidth(), width);
-  }
-  this.minHeight = function () {
-    return Math.max(this.inner.minHeight(), height);
-  }
-  this.draw = function(width, height) {
-    return this.inner.draw(width, height);
+// function StretchCell(inner, width, height) {
+//   this.inner = inner;
+//   this.minWidth = function () {
+//     return Math.max(this.inner.minWidth(), width);
+//   }
+//   this.minHeight = function () {
+//     return Math.max(this.inner.minHeight(), height);
+//   }
+//   this.draw = function(width, height) {
+//     return this.inner.draw(width, height);
+//   }
+// }
+
+// sequence interface
+
+function logFive(sequence) {
+  for (var i = 0; i < 5; i++) {
+    console.log(sequence.current());
+    if(!sequence.next()) { break; }
   }
 }
+
+function ArraySeq(array) {
+  this.array = array;
+  this.pointer = 0;
+}
+
+ArraySeq.prototype.current = function () {
+  return this.array[this.pointer];
+}
+
+ArraySeq.prototype.next = function () {
+  this.pointer++;
+  if (this.pointer >= this.array.length) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function RangeSeq(start, stop) {
+  var array = [];
+  for (var i = start; i < stop; i++) {
+    array.push(i);
+  }
+  ArraySeq.call(this, array)
+}
+
+RangeSeq.prototype = Object.create(ArraySeq.prototype);
+
+logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+logFive(new RangeSeq(100, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
